5f9ceb9bbc4d2a16ab742a45d739b2eb62b61406
MyAxis <timon03032007@gmail.com>
2025-11-19 15:24:04 +0300

    Создали класс Enemy, который хранит данные о противнике

--- RogaliekePractice/Program.cs
+++ RogaliekePractice/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+using System.Diagnostics;
+
+class russia_roguelik
+{
+    static Random random = new Random();
+    class Enemy
+    {
+        public string Name;
+        public int HP;
+        public int Attack;
+
+        public Enemy(string name, int hp, int attack)
+        {
+            Name = name;
+            HP = hp;
+            Attack = attack;
+        }
+
+        static void Main()
+        {
+            
+            
+        }
+
+
+    }
+}
